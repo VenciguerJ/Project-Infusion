145b6f0ef7f8eff37e66c8578b231b0039590dbc
VenciguerJ <blazeboygamer24@gmail.com>
2024-02-06 12:36:43 -0300

    a

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,47 @@
 (function(){
     'use strict'
     
-    var $menuButtom = document.querySelector('#js-menu-buttom')
-    var $body = document.querySelector('body');
 
-    $menuButtom.addEventListener('click', mostraMenu)
-    window.addEventListener('load', confereMenuAtivado)
+    //DOM
+
+    var $menuButtom = document.querySelector('#js-menu-buttom');
+    var $body = document.querySelector('body');
+    var $menu = document.querySelector('.menu-list');
+
+
+    // EVENTOS
+
+    $menuButtom.addEventListener('click', alteraMenu)
+    window.addEventListener('resize', confereMenuAtivado)
+
+
+    // FUNÇÕES DE EVENTO
 
     function confereMenuAtivado(){
-        if(document.querySelector('.start-menu-effect') == null){
+        const LARGURA_MENU_DESKTOP = 1024;
+        var widthWindow = parseFloat(getComputedStyle($body).width);
+        console.log(widthWindow)
+
+        if(widthWindow > LARGURA_MENU_DESKTOP){
+            console.log('maior: ', widthWindow);
+        }
+        else if(widthWindow < LARGURA_MENU_DESKTOP){
+            alteraMenu();
+        }
+
+    }
+    function alteraMenu(){
+        var isActive = document.querySelector('.start-menu-effect');
+
+        if(isActive == null){
             $body.classList.add('start-menu-effect');
         }
-        else if(document.querySelector('.start-menu-effect') != null){
+        else if(isActive != null){
             $body.classList.remove('start-menu-effect');
         }
+
     }
-    function mostraMenu(){
-        $body.classList.toggle('start-menu-effect');
-    }
+
+    //testes
 
 })();
