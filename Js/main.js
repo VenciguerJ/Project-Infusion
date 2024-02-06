97d45d6cb9ad51e652f8980c9256e5d8d7f4783b
Jean~ <jeanpedrosilva24@gmail.com>
2024-02-05 22:29:31 -0300

    continuação do commit

--- Js/main.js
+++ Js/main.js
@@ -1,22 +1,73 @@
 (function(){
     'use strict'
     
-    var $menuButtom = document.querySelector('#js-menu-buttom')
+
+    //DOM
+
+    var $menuButtom = document.querySelector('#js-menu-buttom');
     var $body = document.querySelector('body');
 
-    $menuButtom.addEventListener('click', mostraMenu)
-    window.addEventListener('load', confereMenuAtivado)
+    var $next =document.querySelectorAll('.js-next');
+    var $prev =document.querySelectorAll('.js-prev');
+
+
+    // EVENTOS
+
+    $menuButtom.addEventListener('click', alteraMenu)
+    window.addEventListener('resize', confereMenuAtivado)
+
+
+    
+    $next[0].addEventListener('click', showNextContent); 
+    $prev[0].addEventListener('click', showNextContent);
+
+
+    // FUNÇÕES DE EVENTO
 
     function confereMenuAtivado(){
-        if(document.querySelector('.start-menu-effect') == null){
+        const LARGURA_MENU_DESKTOP = 1024;
+        var widthWindow = parseFloat(getComputedStyle($body).width);
+
+        if(widthWindow < LARGURA_MENU_DESKTOP){
+            alteraMenu();
+        }
+
+    }
+    function alteraMenu(){
+        var isActive = document.querySelector('.start-menu-effect');
+
+        if(isActive == null){
             $body.classList.add('start-menu-effect');
         }
-        else if(document.querySelector('.start-menu-effect') != null){
+        else if(isActive != null){
             $body.classList.remove('start-menu-effect');
         }
+
     }
-    function mostraMenu(){
-        $body.classList.toggle('start-menu-effect');
+
+
+    var actualPosition = 0
+    function showNextContent(evt){
+        var laptop = $next[0] || $prev[0];
+        var text = $next[1];
+        console.log(evt.currentTarget);
+
+        if(evt.currentTarget == laptop){
+            //evento Laptop
+            var $laptopFigure = document.querySelectorAll('.js-laptop-figure');
+            console.log(evt.currentTarget);
+        }
+        if(evt.currentTarget == text){
+            // evento text
+        }
     }
+    function showPrevContent(evt){
+        var laptop = $prev[0];
+        var text = $prev[1]
+        var actualPosition = 0
+    }
+
+    //testes
+
 
 })();
