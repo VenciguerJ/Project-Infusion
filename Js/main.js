2d88d93ea40f8eaf63cc5f2ca4a77943b8bd0927
VenciguerJ <blazeboygamer24@gmail.com>
2024-02-07 17:56:40 -0300

    slides

--- Js/main.js
+++ Js/main.js
@@ -2,24 +2,11 @@
     'use strict'
     
 
-    //DOM
-
-    var $menuButtom = document.querySelector('#js-menu-buttom');
 
     //DOM
 
     var $menuButtom = document.querySelector('#js-menu-buttom');
     var $body = document.querySelector('body');
-    var $menu = document.querySelector('.menu-list');
-
-
-    // EVENTOS
-
-    $menuButtom.addEventListener('click', alteraMenu)
-    window.addEventListener('resize', confereMenuAtivado)
-
-
-    // FUNÇÕES DE EVENTO
     var $next =document.querySelectorAll('.js-next');
     var $prev =document.querySelectorAll('.js-prev');
 
@@ -29,23 +16,20 @@
     $menuButtom.addEventListener('click', alteraMenu)
     window.addEventListener('resize', confereMenuAtivado)
 
-
-    
-    $next[0].addEventListener('click', showNextContent); 
-    $prev[0].addEventListener('click', showNextContent);
-
+    for (let i = 0;$next[i]; i++) {
+        $next[i].addEventListener('click', showNextContent);
+    }
+    for (let i = 0;$prev[i]; i++) {
+        $prev[i].addEventListener('click', showNextContent);
+    }
 
     // FUNÇÕES DE EVENTO
 
     function confereMenuAtivado(){
         const LARGURA_MENU_DESKTOP = 1024;
         var widthWindow = parseFloat(getComputedStyle($body).width);
-        console.log(widthWindow)
-
-        if(widthWindow > LARGURA_MENU_DESKTOP){
-            console.log('maior: ', widthWindow);
-        }
-        else if(widthWindow < LARGURA_MENU_DESKTOP){
+        
+        if(widthWindow < LARGURA_MENU_DESKTOP){
             alteraMenu();
         }
 
@@ -62,32 +46,50 @@
 
     }
 
-    }
-
-
-    var actualPosition = 0
     function showNextContent(evt){
-        var laptop = $next[0] || $prev[0];
+        var laptop = $next[0];
         var text = $next[1];
-        console.log(evt.currentTarget);
-
         if(evt.currentTarget == laptop){
             //evento Laptop
             var $laptopFigure = document.querySelectorAll('.js-laptop-figure');
-            console.log(evt.currentTarget);
+            var actualPosition = returnShowedPosition($laptopFigure);
+            actualPosition = alteraSlide('proximo', $laptopFigure, actualPosition);
         }
         if(evt.currentTarget == text){
             // evento text
+            var $textPartition = document.querySelectorAll('.text-partition');
+            console.log($textPartition[1] != document.querySelector('.hidden'))
+            var actualPosition = returnShowedPosition($textPartition);
+            actualPosition = alteraSlide('proximo', $textPartition, actualPosition);
         }
     }
+
     function showPrevContent(evt){
         var laptop = $prev[0];
         var text = $prev[1]
         var actualPosition = 0
     }
 
-    //testes
+    function returnShowedPosition(positions){ // mostra o index que não possui a classe hidden
+        for(var i=0;positions[i];i++){
+            if(positions[i] != document.querySelector('.hidden')){
+                var showedPosition = i;
+            }
+        }
+        return showedPosition
+    }
+    function alteraSlide(direcao, arr, position){
+        if(direcao === 'proximo'){
+            arr[position].classList.add('hidden');
+            position ++;
 
+            if(position >= arr.length){
+                position = 0;
+            }
+            arr[position].classList.remove('hidden');
+        }
+        return position;
+    }
 
     //testes
 
